49801c29dccd130f5939206c3d1d05018ef335d1
Kaj <kaj@kajbonfils.dk>
2022-10-26 13:08:53 +0200

    added test project

---
+++ MinimalApiTestTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using MinimalApiTest.Request;
+using Xunit;
+
+namespace MinimalApiTestTests
+{
+    public class CreateCarModelHandlerTest
+    {
+        [Fact]
+        public async Task Handle()
+        {
+            var target = new CreateCarModelHandler();
+
+            var actual = await target.Handle(new CreateCarModelRequest("Foo"), new CancellationToken());
+
+            Assert.Equal(false, actual.IsSuccess);
+        }
+    }
+}
